1897e3620713a96bcdad43904c5e9e83565edcb8
John Spicer <jspicer@stevens.edu>
2016-04-19 14:14:15 -0400

    file upload and profile update

---
+++ profile.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="styles.css"></link>
+        <link rel="icon" type="image/png" href="favicon.jpg">
+        <title>DuckFuck</title>
+    </head>
+   
+    <body>
+        <div id="header">
+            <h1 id="headertext">DuckFuck</h1>
+            <input type="submit" id="signin" value="Sign In" onclick="location.href='www.duckfuck.cf/index.php';">
+	    <input type="submit" id="logout" value="Log Out" onclick="location.href='www.duckfuck.cf/logout.php';">
+        </div>
+
+	<div id="form">
+		<form>
+	</div>
