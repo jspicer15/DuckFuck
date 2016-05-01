5241715458e5ba0ed3fa67a06559c2cfbb88bf0a
NuclearFej <j@fej.io>
2016-05-01 04:09:09 -0400

    Better signup page. CSS needs work.

--- signup.php
+++ signup.php
@@ -2,7 +2,6 @@
 	require "db.php";
 
 	$db = connectToDB();
-	print_r($db);
 
 	$FirstName = $_POST['FirstName'];
 	$LastName = $_POST['LastName'];
@@ -16,38 +15,30 @@
 	$statement = $db->prepare('INSERT INTO users (first, last, email, password, activation) VALUES (?, ?, ?, ?, ?)');
 
 	$statement->execute(array($FirstName, $LastName, $email, $hash, $activation_hash));
-	//print_r($statement->errorInfo);
-
-	//echo "Thank you for signing up. You will receive a confirmation email shortly with a link to activate your account. Thank you again! You will automatically be redirected. If your browser does not redirect <a href= http://www.duckfuck.cf> Click Here </a>";
-	//echo '<script type="text/javascript">window.location = "http://www.duckfuck.cf"</script>';
-	/*
-    $to = $email; // Send email to our user
-    $subject = 'DuckFuck Account Activation'; // Give the email a subject
-    $message = '
-
-    Thanks for signing up for DuckFuck!
-    Your account has been created, you can login after you have activated your account by pressing the url below.
-
-
-    Please click this link to activate your account:
-    http://www.duckfuck.whatever/verify_account.php?email='.$email.'&hash='.$hash.'
-
-    ';
-
-    $headers = 'From:noreply@duckfuck.whatever' . "\r\n"; // Set from headers
-    mail($to, $subject, $message, $headers); // Send our email
-
-	echo "Thank you for signing up. You will receive a confirmation email shortly with a link to activate your account. Thank you again! You will automatically be redirected. If your browser does not redirect <a href= http://www.duckfuck.cf> Click Here </a>";
-	sleep(2000);
-	echo '<script type="text/javascript">
-	window.location = "http://www.duckfuck.cf"
-	</script>';
-	}
-	else
-	{
-		echo "Error updating record: " . $conn->error;
-	}
-
-	*/
-
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="styles.css"></link>
+        <link rel="icon" type="image/png" href="favicon.png">
+        <title>DuckFuck</title>
+    </head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php"><img src="logo/df-logo.svg" alt="DuckFuck" id="headerlogo"></a></li>
+                <li id="navtitle">Welcome to DuckFuck</li>
+            </ul>
+        </nav>
+    </header>
+    <p class="signedup">
+    	Thank you for signing up! You will receive a confirmation email shortly with a link to activate your account.
+    </p>
+    <p class="signedup">
+    	You may close this tab, as clicking the link in your email will open another one.
+    </p>
+</body>
+</html>
