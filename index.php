11203a8f3f4a4d8822011d9faec22656edcb19ed
NuclearFej <j@fej.io>
2016-04-30 20:59:43 -0400

    Add favicon.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <link rel="stylesheet" type="text/css" href="styles.css"></link>
-        <link rel="icon" type="image/png" href="favicon.jpg">
+        <link rel="icon" type="image/png" href="favicon.png">
         <title>DuckFuck</title>
     </head>
    
