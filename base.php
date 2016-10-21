e2a88627712d5870a99c805675bb547074f21f8e
John Spicer <jspicer@stevens.edu>
2016-10-21 16:54:45 -0400

    Update base.php

--- base.php
+++ base.php
@@ -2,10 +2,10 @@
 session_start();
  
 //SQL CONNECTION
-$servername = "sql201.freecluster.eu";
-$username = "fceu_17834029";
-$password = "duckfuck";
-$dbname = "fceu_17834029_users";
+$servername = "#####";
+$username = "#######";
+$password = "######";
+$dbname = "######";
  
 mysql_connect($servername, $username, $password) or die("MySQL Error: " . mysql_error());
 mysql_select_db($dbname) or die("MySQL Error: " . mysql_error());
